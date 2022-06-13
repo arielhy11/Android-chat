5f92824088ec67cd65e2f82eabe4b6b137c2c663
arielhy11 <arielhy11@gmail.com>
2022-06-13 16:14:49 +0300

    registered via Firebase. finished third part

--- app/src/main/java/com/example/android_chat/activities/activities/RegisterActivity.java
+++ app/src/main/java/com/example/android_chat/activities/activities/RegisterActivity.java
@@ -1,22 +1,34 @@
 package com.example.android_chat.activities.activities;
 
-import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Toast;
 
-import com.example.android_chat.R;
+import com.example.android_chat.activities.firebase.utilities.ManagerPreferences;
 import com.example.android_chat.databinding.ActivityRegisterBinding;
-import com.example.android_chat.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.regex.Pattern;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.prefs.PreferenceChangeEvent;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ActivityRegisterBinding binding;
+    private ManagerPreferences managerPreferences;
     private String rawImage;
 
     @Override
@@ -24,6 +36,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        managerPreferences = new ManagerPreferences(getApplicationContext());
         setListeners();
     }
 
@@ -33,6 +46,11 @@ public class RegisterActivity extends AppCompatActivity {
             if (isLegal()){
                 register();
             }
+        });
+        binding.wrapProfilePic.setOnClickListener(e->{
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+            pickImage.launch(intent);
         });
     }
 
@@ -68,7 +86,69 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    public void register(){
+    private void whileLoading(Boolean isLoading){
+        if(isLoading){
+            binding.buttonRegister.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonRegister.setVisibility(View.VISIBLE);
+        }
+    }
 
+    private String workRawImage(Bitmap bitmap){
+        int width = 140;
+        int height = bitmap.getHeight() * width / bitmap.getWidth();
+        Bitmap showBitmap = Bitmap.createScaledBitmap(bitmap,width,height,false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        showBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] byteArr = byteArrayOutputStream.toByteArray();
+        return android.util.Base64.encodeToString(byteArr, Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK){
+                    if (result.getData() != null){
+                        Uri uri = result.getData().getData();
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(uri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.ProfilePic.setImageBitmap(bitmap);
+                            binding.ProfilePicText.setVisibility(View.GONE);
+                            rawImage = workRawImage(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+
+    public void register(){
+        whileLoading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put("name", binding.inputUsername.getText().toString());
+        user.put("email", binding.inputEmail.getText().toString());
+        user.put("password", binding.inputPassword.getText().toString());
+        user.put("image", rawImage);
+        database.collection("users").add(user).addOnSuccessListener(documentReference -> {
+            whileLoading(false);
+            managerPreferences.putBoolean("isSignedIn", true);
+            managerPreferences.putString("userId", documentReference.getId());
+            managerPreferences.putString("name", binding.inputUsername.getText().toString());
+            managerPreferences.putString("image", rawImage);
+
+            maketext("Success");
+            Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
+        }).addOnFailureListener(exception ->{
+            whileLoading(false);
+            maketext(exception.getMessage());
+        });
     }
 }
